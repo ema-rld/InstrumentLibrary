81529ea918f881980fe2421bceaeef49f554e9c5
Emmanuelle ROLLAND <e.rolland78@gmail.com>
2021-11-10 16:44:41 +0000

    InstrumentLibrary

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using InstrumentLibrary.Models;
+using System;
+
+namespace InstrumentLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instrument item = new Instrument();
+            Guitar myGuitar = new Guitar("Gibson");
+            Mandolin myMandolin = new Mandolin("Kentucky KM-140");
+            Ukulele myUkulele = new Ukulele("Ortega");
+
+            myUkulele.serialNumber = "abc123";
+
+            myGuitar.Play();
+            myMandolin.Play();
+            myUkulele.Play();
+        }
+    }
+}
